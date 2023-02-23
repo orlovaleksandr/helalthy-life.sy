86b92955dafc976ec1be171c00b0246f3ce47ea1
Aleksandr Orlov <aleksandr.orlov@internetfusion.co.uk>
2023-02-23 17:09:51 +0300

    created profile pages

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ProfileEditFormType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileController extends AbstractController
+{
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    #[Route('/profile', name: 'main_profile')]
+    public function index(): Response
+    {
+        return $this->render('main/profile/index.html.twig');
+    }
+
+    #[Route('/profile/edit', name: 'main_profile-edit')]
+    public function edit(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileEditFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->userRepository->save($form->getData(), true);
+
+            return $this->redirectToRoute('main_profile');
+        }
+
+        return $this->render('main/profile/edit.html.twig', [
+            'profileEditForm' => $form->createView()
+        ]);
+    }
+}
--- src/Form/EditProductFormType.php
+++ src/Form/EditProductFormType.php
@@ -4,18 +4,35 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class EditProductFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('price')
-            ->add('quantity')
-            ->add('description')
+            ->add('title', TextType::class, [
+                'label' => 'Title (From class)',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(message: 'Should be filled')
+                ]
+            ])
+            ->add('price', NumberType::class, [
+                'label' => 'Price (From class)',
+                'scale' => 2,
+                'html5' => true,
+                'attr' => [
+                    'step' => 0.01
+                ]
+            ])
+            ->add('quantity', IntegerType::class)
+            ->add('description', TextType::class)
             ->add('isPublished')
             ->add('isDeleted')
         ;
